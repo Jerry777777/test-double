0d0671a688ba5d802464d89867dc1c483bc0f08d
Jerry777777 <491818175@qq.com>
2020-09-16 01:42:45 +0800

    [xiawenhao] refactor: mock and stub test use mockito

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,11 +3,14 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
@@ -18,12 +21,12 @@ public class SecurityCenterTest {
     private DoorPanel mockDoorPanel;
     @BeforeEach
     public void setUp() {
-        mockDoorPanel= new DoorPanel();
-        securityCenter = new SecurityCenter(mockDoorPanel);
+
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
         securityCenter.switchOn();
+        verify(mockDoorPanel).close();
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -3,15 +3,29 @@ package stub;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
 
+    @InjectMocks
+    private GradeService gradeService;
+
+    @Mock
+    private GradeSystem mockGradeSystem ;
+
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        GradeService gradeService = new GradeService(new StubGradeSystem());
+        List<Double> list = Arrays.asList(90.0, 80.0, 100.0);
+        when(mockGradeSystem.gradesFor(0)).thenReturn(list);
         double result = gradeService.calculateAverageGrades(0);
         Assertions.assertEquals(90.0, result);
     }
